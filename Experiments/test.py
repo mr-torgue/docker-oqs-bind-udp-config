7ea2ad9efc03ef85b92dcc2b061c2d518b776b6f
Dev <me+github@folmer.info>
2026-02-05 16:59:44 +1100

    added test

--- Experiments/test.py
+++ Experiments/test.py
@@ -1,14 +1,25 @@
 from ripe.atlas.cousteau import AtlasStream
 
+def on_result_response(*args):
+    """
+    Function that will be called every time we receive a new result.
+    Args is a tuple, so you should use args[0] to access the real message.
+    """
+    print(args[0])
+
 atlas_stream = AtlasStream()
 atlas_stream.connect()
 
-stream_parameters = {"enrichProbes": True}
-atlas_stream.subscribe(stream_type="probestatus", **stream_parameters)
+# Bind function we want to run with every result message received
+atlas_stream.bind("atlas_result", on_result_response)
 
-# Iterate over the incoming results for 5 seconds
-for event_name, payload in atlas_stream.iter(seconds=5):
-    print(event_name, payload)
+# Subscribe to new stream for 1001 measurement results
+stream_parameters = {"msm": 1001}
+atlas_stream.subscribe(stream_type="result", **stream_parameters)
+
+# Process incoming events for 5 seconds, calling the callback defined above.
+# Make sure you have this line after you start *all* your streams
+atlas_stream.timeout(seconds=5)
 
 # Shut down everything
 atlas_stream.disconnect()
