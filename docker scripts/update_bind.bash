e24d52cfc39c1746675c2c7c6e913af3921f1f12
Dev <me+github@folmer.info>
2026-02-09 14:55:51 +1100

    added debug to the update script

--- docker scripts/update_bind.bash
+++ docker scripts/update_bind.bash
@@ -3,8 +3,19 @@
 updates the OQS-BIND code on the container
 '
 release=v1.2.1
-if [ "$#" -eq 1 ]; then
+debug=false
+if [ "$#" -eq 2 ]; then
     release=$1
+    if [ "$2" == "--debug" ]; then
+        debug=true
+    fi
+fi
+if [ "$#" -eq 1 ]; then
+    if [ "$1" == "--debug" ]; then
+        debug=true
+    else
+        release=$1
+    fi
 fi
 
 cd /OQS-bind
@@ -12,6 +23,10 @@ git fetch
 git checkout $release
 git pull
 autoreconf -fi
-RUN CFLAGS="$CFLAGS -O0 -g" ./configure 
+if [ "$debug" = true ]; then
+    RUN CFLAGS="$CFLAGS -O0 -g -pg" ./configure
+else
+    RUN CFLAGS="$CFLAGS" ./configure
+fi
 make
 make install
