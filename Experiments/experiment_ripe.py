7e3bc7d3ccff66f3e498dedb7de842f54e588ec4
Dev <me+github@folmer.info>
2026-02-23 15:21:59 +1100

    added excludes

--- Experiments/experiment_ripe.py
+++ Experiments/experiment_ripe.py
@@ -46,7 +46,8 @@ def run_measurement(nr_sources, resolver, domain, description, label, algorithm,
             type="area",
             value="WW",
             requested=nr_sources,
-            tags={"include":["system-ipv4-works", "system-ipv4-stable-30d", "system-v2"]}
+            tags={"include":["system-ipv4-works", "system-ipv4-stable-30d"], "exclude": ["system-v2", "system-v1"]}
+            #tags={"include":["system-ipv4-works", "system-ipv4-stable-30d", "system-v2"]}
         )
     else:
         print("Using %d random probes from country %s" % (nr_sources, country))
@@ -54,7 +55,8 @@ def run_measurement(nr_sources, resolver, domain, description, label, algorithm,
             type="country",
             value=country,
             requested=nr_sources,
-            tags={"include":["system-ipv4-works", "system-ipv4-stable-30d", "system-v2"]}
+            tags={"include":["system-ipv4-works", "system-ipv4-stable-30d"], "exclude": ["system-v2", "system-v1"]}
+            #tags={"include":["system-ipv4-works", "system-ipv4-stable-30d", "system-v2"]}
         )
     measurement = Dns(
         af=4,
@@ -175,15 +177,16 @@ def run_experiment(nr_sources, nr_queries, resolver, domain, description, label,
             type="area",
             value="WW",
             requested=nr_sources,
-            # tags={"include":["system-ipv4-works", "system-ipv4-stable-30d"], "exclude": ["system-v2", "system-v1"]}
-            tags={"include":["system-ipv4-works", "system-ipv4-stable-30d", "system-v2"]}
+            tags={"include":["system-ipv4-works", "system-ipv4-stable-30d"], "exclude": ["system-v2", "system-v1"]}
+            #tags={"include":["system-ipv4-works", "system-ipv4-stable-30d", "system-v2"]}
         )
     else:
         source = AtlasSource(
             type="country",
             value=country,
             requested=nr_sources,
-            tags={"include":["system-ipv4-works", "system-ipv4-stable-30d", "system-v2"]}
+            tags={"include":["system-ipv4-works", "system-ipv4-stable-30d"], "exclude": ["system-v2", "system-v1"]}
+            #tags={"include":["system-ipv4-works", "system-ipv4-stable-30d", "system-v2"]}
         )
     prefix, suffix = domain.split(".", 1)
     measurements=[]
